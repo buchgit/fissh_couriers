b178f80349fe9a7df54d262ea258d62ff41ca0a1
buchgit <buchgit@gmail.com>
2022-09-03 00:54:27 +0300

    add shared preferences

--- app/src/main/java/com/example/fissh_couriers/model/sharedPreferences/AppSharedPreferences.kt
+++ app/src/main/java/com/example/fissh_couriers/model/sharedPreferences/AppSharedPreferences.kt
@@ -2,7 +2,7 @@ package com.example.fissh_couriers.model.sharedPreferences
 
 interface AppSharedPreferences {
 
-    //в SharedPreferences будет хранить токен
+    //в SharedPreferences будет храниться токен
     fun getToken(): String?
     fun setToken(token: String?)
 
